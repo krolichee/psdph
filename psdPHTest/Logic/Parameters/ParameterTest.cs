61e8b4c7b2f80732229ae0171a7bd3a5ec870b87
krolichee <115797367+krolichee@users.noreply.github.com>
2025-06-08 22:58:53 +0800

    добавлена проверка соответствия документа шаблону, сохранение результатов в отдельные файлы, обработка несохранённого документа, прототип мета-параметров, переименование ReflectionParameter и ReflectionSetup

--- psdPHTest/Logic/Parameters/ParameterTest.cs
+++ psdPHTest/Logic/Parameters/ParameterTest.cs
@@ -13,28 +13,28 @@ namespace psdPHTest.Logic
 {
     namespace Parameters
     {
-        [TestClass]
-        public class ParameterTest
+    [TestClass]
+    public class ParameterTest
+    {
+        [TestMethod]
+        public void compileTest()
         {
-            [TestMethod]
-            public void compileTest()
-            {
-                var blob = Blob.PathBlob("...");
-                var parameter = new StringParameter() { Name = "надпись" };
-                var textLeaf = new TextLeaf() { LayerName = "text" };
+            var blob = Blob.PathBlob("...");
+            var parameter = new StringParameter() { Name = "надпись" };
+            var textLeaf = new TextLeaf() { LayerName = "text" };
                 var rule = new TextAssignRule(blob) { TextLeaf = textLeaf, Parameter = parameter, Condition = new DummyCondition() };
-                blob.AddChild(textLeaf);
-                blob.RuleSet.AddRule(rule);
-                blob.Parameters.Add(parameter);
-                string tempFilePath = Path.GetTempFileName() + ".psd"; // или с нужным расширением
+            blob.AddChild(textLeaf);
+            blob.RuleSet.AddRule(rule);
+            blob.Parameters.Add(parameter);
+            string tempFilePath = Path.GetTempFileName() + ".psd"; // или с нужным расширением
                 File.WriteAllBytes(tempFilePath, Properties.TestResources.Basic);
-                var doc = PhotoshopWrapper.OpenDocument(tempFilePath);
-                parameter.Value = "hzzzz";
-                doc.Rollback();
-                blob.Apply(doc);
-                var result = doc.GetLayerByName(textLeaf.LayerName).TextItem.Contents;
-                Assert.IsTrue(result == parameter.Value);
-            }
+            var doc = PhotoshopWrapper.OpenDocument(tempFilePath);
+            parameter.Value = "hzzzz";
+            doc.Rollback();
+            blob.Apply(doc);
+            var result = doc.GetLayerByName(textLeaf.LayerName).TextItem.Contents;
+            Assert.IsTrue(result == parameter.Value);
         }
     }
+}
 }
