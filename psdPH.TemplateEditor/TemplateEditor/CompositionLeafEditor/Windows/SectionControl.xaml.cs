93f6273a470a561dc93d91e2d8e230b6724ca9cd
krolichee <115797367+krolichee@users.noreply.github.com>
2025-07-16 20:21:02 +0800

    ...

--- psdPH.TemplateEditor/TemplateEditor/CompositionLeafEditor/Windows/SectionControl.xaml.cs
+++ psdPH.TemplateEditor/TemplateEditor/CompositionLeafEditor/Windows/SectionControl.xaml.cs
@@ -2,51 +2,59 @@
 using System.Windows.Media;
 using System.Windows;
 
-namespace psdPH.TemplateEditor
+namespace psdPH.TemplateEditor { 
+public partial class SectionControl : UserControl
 {
-    public partial class SectionControl : UserControl
+    public SectionControl()
     {
-        public SectionControl()
-        {
-            InitializeComponent();
-        }
-
-        public static readonly DependencyProperty SectionTitleProperty =
-            DependencyProperty.Register("SectionTitle", typeof(string), typeof(SectionControl));
-
-        public static readonly DependencyProperty SectionIconSourceProperty =
-            DependencyProperty.Register("SectionIconSource", typeof(ImageSource), typeof(SectionControl));
-
-        public static readonly DependencyProperty SectionBorderThicknessProperty =
-            DependencyProperty.Register("SectionBorderThickness", typeof(Thickness), typeof(SectionControl),
-                new PropertyMetadata(new Thickness(1, 1, 1, 1)));
-
-        public static readonly DependencyProperty SectionBorderBrushProperty =
-            DependencyProperty.Register("SectionBorderBrush", typeof(Brush), typeof(SectionControl),
-                new PropertyMetadata(Brushes.Black));
-
-        public string SectionTitle
-        {
-            get => (string)GetValue(SectionTitleProperty);
-            set => SetValue(SectionTitleProperty, value);
-        }
-
-        public ImageSource SectionIconSource
-        {
-            get => (ImageSource)GetValue(SectionIconSourceProperty);
-            set => SetValue(SectionIconSourceProperty, value);
-        }
-
-        public Thickness SectionBorderThickness
-        {
-            get => (Thickness)GetValue(SectionBorderThicknessProperty);
-            set => SetValue(SectionBorderThicknessProperty, value);
-        }
-
-        public Brush SectionBorderBrush
-        {
-            get => (Brush)GetValue(SectionBorderBrushProperty);
-            set => SetValue(SectionBorderBrushProperty, value);
-        }
+        InitializeComponent();
+        this.DataContext = this;
     }
-}
+
+    public static readonly DependencyProperty TitleProperty =
+        DependencyProperty.Register("Title", typeof(string), typeof(SectionControl));
+
+    public static readonly DependencyProperty IconSourceProperty =
+        DependencyProperty.Register("IconSource", typeof(ImageSource), typeof(SectionControl));
+
+    public static readonly DependencyProperty BorderThicknessProperty =
+        DependencyProperty.Register("BorderThickness", typeof(Thickness), typeof(SectionControl),
+            new PropertyMetadata(new Thickness(1, 1, 1, 1)));
+
+    public static readonly DependencyProperty BorderBrushProperty =
+        DependencyProperty.Register("BorderBrush", typeof(Brush), typeof(SectionControl),
+            new PropertyMetadata(Brushes.Black));
+
+    public string Title
+    {
+        get { return (string)GetValue(TitleProperty); }
+        set { SetValue(TitleProperty, value); }
+    }
+
+    public ImageSource IconSource
+    {
+        get { return (ImageSource)GetValue(IconSourceProperty); }
+        set { SetValue(IconSourceProperty, value); }
+    }
+
+    public Thickness BorderThickness
+    {
+        get { return (Thickness)GetValue(BorderThicknessProperty); }
+        set { SetValue(BorderThicknessProperty, value); }
+    }
+
+    public Brush BorderBrush
+    {
+        get { return (Brush)GetValue(BorderBrushProperty); }
+        set { SetValue(BorderBrushProperty, value); }
+    }
+
+    public object Content
+    {
+        get { return GetValue(ContentProperty); }
+        set { SetValue(ContentProperty, value); }
+    }
+
+    public static new readonly DependencyProperty ContentProperty =
+        DependencyProperty.Register("Content", typeof(object), typeof(SectionControl));
+}}
