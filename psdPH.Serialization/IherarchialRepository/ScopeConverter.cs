7f83338764dd737118bb9eaa6fe94b2d99af7b40
krolichee <115797367+krolichee@users.noreply.github.com>
2025-11-06 22:25:21 +0800

    refactor: untested IherarchialRepository

---
+++ psdPH.Serialization/IherarchialRepository/ScopeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace psdPH.Serialization
+{
+    class ScopeConverter
+    {
+        public static ConversionContext ConvertDtoScope(DtoScope dtoScope)
+        {
+            var identities = new List<Identity>();
+            var references = new List<PendingReference>();
+            foreach (var item in dtoScope.scope)
+            {
+                DtoConverter converter = DtoConvertersRegistry.GetForDto(item);
+                var identity = converter.GetIdentity(item, out PendingReference[] pReferences);
+                identities.Add(identity);
+                references.AddRange(pReferences);
+            }
+            return new ConversionContext(new IdentityMap(identities.ToArray()),references.ToArray());
+        }
+    }
+}
