22e465172c09b4e0c0f2db2132452e806e4d3266
Lev <lev-zyryanov@mail.ru>
2026-01-10 22:37:25 +0800

    untested DateLetView

---
+++ psdPH.LetViews/Date/DateLetView.cs
@@ -0,0 +1,19 @@
+﻿using psdPH.Lets;
+using psdPH.Lets.Core;
+using psdPH.LetViews.Check;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace psdPH.LetViews.Date
+{
+    internal class DateLetView : LetView
+    {
+        public DateLetView(Let let) : base(let)
+        {
+            control = new DateLetViewControl(let) { };
+        }
+    }
+}
